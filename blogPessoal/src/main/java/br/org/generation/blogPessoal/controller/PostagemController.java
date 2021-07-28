07e387598a8a12b13bd066ba8a85a96ca81aa841
gabriel8514 <gabrielluizreis936@hotmail.com>
2021-07-28 15:55:29 -0300

    Atualização projeto inclusão de Relacionamento entre tabelas e CRUD

---
+++ blogPessoal/src/main/java/br/org/generation/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,67 @@
+package br.org.generation.blogPessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.generation.blogPessoal.model.Postagem;
+import br.org.generation.blogPessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	//inveja os serviços. 
+	@Autowired
+	private PostagemRepository repository;
+	
+	// essa anotação/metodo retorna tudo que tem no banco de dados por isso é o findAll
+	@GetMapping 
+	public ResponseEntity<List<Postagem>> GetAll (){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	// Essa possibilita a pesquisa por id exemplo http://localhost:8080/postagens/2  e caso não seja encontrado retorna 404-notfound
+	@GetMapping("/{id}") 
+	public ResponseEntity<Postagem> GetById(@PathVariable long id){ 
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build()); 
+	}
+	
+	// esse metodo permite pesquisar pela discrição como se fosse o Like no SQL.
+	@GetMapping ("/titulo/{titulo}")  
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo)); 
+	}
+	
+	// esse metodo insere postagens no banco de dados. 
+	@PostMapping 
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	// esse metodo faz alterações nas postagens. 
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	// deleta 
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id); 
+	}
+}
